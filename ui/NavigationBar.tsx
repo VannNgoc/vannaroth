478c91096523ce9626308e40d589d85f6404b4d6
Vannaroth Ngoc <vannarothngoc@gmail.com>
2026-09-21 00:25:33 -0400

    Disable unfinished pages

--- ui/NavigationBar.tsx
+++ ui/NavigationBar.tsx
@@ -2,12 +2,12 @@ import {HamburgerMenu} from "./HamburgerMenu";
 import Link from "next/link";
 import Image from "next/image";
 
-const navLinks = [
-    // { href: "/", label: "Home" },
-    // { href: "/about-me", label: "About Me" },
-    // { href: "/projects", label: "Projects" },
-    // { href: "/contact", label: "Contact Me" },
-];
+// const navLinks = [
+//     // { href: "/", label: "Home" },
+//     // { href: "/about-me", label: "About Me" },
+//     // { href: "/projects", label: "Projects" },
+//     // { href: "/contact", label: "Contact Me" },
+// ];
 
 export default function NavigationBar(){
     return(
@@ -19,15 +19,15 @@ export default function NavigationBar(){
                         Vannaroth Ngoc
                     </Link>
                 </div>
-                <HamburgerMenu navLinks={navLinks} />
-                <div className="hidden md:flex md:items-center">
-                    {navLinks.map((link) => (
-                        <Link key={link.href} className="hover:underline mx-2" href={link.href}>
-                            {link.label}
-                        </Link>
-                    ))}
-                    {/*<Link className="btn-primary ml-2" rel="noopener noreferrer" aria-label="Download résumé (opens in a new tab)" target="_blank" href="/resume.pdf">Resume</Link>*/}
-                </div>
+                {/*<HamburgerMenu navLinks={navLinks} />*/}
+                {/*<div className="hidden md:flex md:items-center">*/}
+                {/*    {navLinks.map((link) => (*/}
+                {/*        <Link key={link.href} className="hover:underline mx-2" href={link.href}>*/}
+                {/*            {link.label}*/}
+                {/*        </Link>*/}
+                {/*    ))}*/}
+                {/*    <Link className="btn-primary ml-2" rel="noopener noreferrer" aria-label="Download résumé (opens in a new tab)" target="_blank" href="/resume.pdf">Resume</Link>*/}
+                {/*</div>*/}
             </nav>
         </div>
     )
